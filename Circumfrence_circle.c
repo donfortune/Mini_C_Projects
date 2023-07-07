245ee4ca317802ccb25821908a362581820574ac
donfortune <donfortunet.df@gmail.com>
2023-07-07 16:53:41 +0100

    modified final output message

--- Circumfrence_circle.c
+++ Circumfrence_circle.c
@@ -1,20 +1,16 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int main(){
-
+int main() {
    const double PI = 3.14159;
    double radius;
    double circumference;
 
+   printf("Please enter a radius: ");
+   scanf("%lf", &radius);
 
-   printf("Please enter a radius?");
-   scanf( "%lf", &radius);
    circumference = 2 * radius * PI;
-   printf("The circumfrence of your circle is %.2lf", circumference);
 
+   printf("The circumference of your circle with radius %.2lf is %.2lf\n", radius, circumference);
 
-
-    return 0;
+   return 0;
 }
-
