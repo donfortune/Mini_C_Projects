4b19c4eb137a0a17cb0867caffdbbb75e25e5057
donfortune <donfortunet.df@gmail.com>
2023-07-07 17:20:34 +0100

    added area of a circle

--- Circumfrence_circle.c
+++ Circumfrence_circle.c
@@ -4,13 +4,16 @@ int main() {
    const double PI = 3.14159;
    double radius;
    double circumference;
+   double area;
 
    printf("Please enter a radius: ");
    scanf("%lf", &radius);
 
    circumference = 2 * radius * PI;
+   area = PI * (radius * radius);
 
    printf("The circumference of your circle with radius %.2lf is %.2lf\n", radius, circumference);
+   printf("The area of a circle is %lf", area);
 
    return 0;
 }
