638088b3eda61828a9ed0f65d6d4e6a168f15d32
donfortune <donfortunet.df@gmail.com>
2023-07-14 14:51:28 +0100

    adjuster case sensitivity

--- temperature_converter.c
+++ temperature_converter.c
@@ -7,15 +7,18 @@ int main (){
 
     printf("what is the unit of your temperature:");
     scanf("%c", &unit);
+   
 
-    if (unit == "C"){
-       
+    unit = toupper(unit);
+
+    if (unit == 'C'){
+       printf("The temp is currently in Celcius");
     }
-    else if (unit == "F"){
-
+    else if (unit == 'C'){
+        printf("The temp is currently in faren");
     }
     else{
-        
+        printf("Invalid option");
     }
     return 0;
 }
