8cf2bd8294c52f9141a4b6af8a1e12e214a4cc70
donfortune <donfortunet.df@gmail.com>
2023-07-14 12:44:35 +0100

    set the structure of app

--- temperature_converter.c
+++ temperature_converter.c
@@ -2,6 +2,20 @@
 #include <ctype.h>     /*function helps with working with characters*/
 
 int main (){
+    char unit;
+    float temp;
 
-    
+    printf("what is the unit of your temperature:");
+    scanf("%c", &unit);
+
+    if (unit == "C"){
+       
+    }
+    else if (unit == "F"){
+
+    }
+    else{
+        
+    }
+    return 0;
 }
